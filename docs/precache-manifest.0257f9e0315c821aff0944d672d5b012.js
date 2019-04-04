dcae6a7f2ebfc8248025c02dfc9a2cbecfc3bc5f
ardean <aron.stifani@appklusiv.ch>
2019-04-05 00:07:08 +0200

    fix

---
+++ docs/precache-manifest.0257f9e0315c821aff0944d672d5b012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8883c0277eadbde921dc2a7d3a36f1f8",
+    "url": "/love/static/media/daunpenh.8883c027.woff"
+  },
+  {
+    "revision": "5a68a4e0bd54f918326fdcf96028e3ff",
+    "url": "/love/static/media/daunpenh.5a68a4e0.ttf"
+  },
+  {
+    "revision": "34914b2a4cc44b98d67e",
+    "url": "/love/static/js/runtime~main.8def1a0a.js"
+  },
+  {
+    "revision": "daf6dae1f8407468cdf9",
+    "url": "/love/static/js/main.81e0da34.chunk.js"
+  },
+  {
+    "revision": "5a65d945e756a74a7934",
+    "url": "/love/static/js/2.89548925.chunk.js"
+  },
+  {
+    "revision": "daf6dae1f8407468cdf9",
+    "url": "/love/static/css/main.a04fbd17.chunk.css"
+  },
+  {
+    "revision": "022bca0621dd0fe7818991d1ba8250de",
+    "url": "/love/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5be2d518b59ab7d8f11dd3532389f41.js"
+  "/love/precache-manifest.0257f9e0315c821aff0944d672d5b012.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/love/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
